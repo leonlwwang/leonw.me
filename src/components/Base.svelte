ccc5114f620ff823eb405f50af2e6271dfe4586e
Leon Wang <leonw2@illinois.edu>
2022-12-18 02:41:31 -0600

    css styling

---
+++ src/components/Base.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Header from "./Header.svelte";
+	import Footer from "./Footer.svelte";
+    export let title;
+</script>
+
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width" />
+		<link rel="icon" type="image/x-icon" href="favicons/favicon.ico" />
+		<title>{title}</title>
+	</head>
+	<body class="bg-base">
+		<Header/>
+		<main>
+			<slot />
+			<div>
+				<p class="text-center">{title.toLowerCase()} page! in progress.</p>
+			</div>
+		</main>
+		<Footer/>
+	</body>
+</html>
---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<div>
+    <nav class="flex justify-between p-6 md:p-8">
+        <a class="flex justify-start items-center text-center w-10 h-10" href="/">
+            <img src="../src/components/home.svg" alt="home">
+        </a>
+        <ul class="flex justify-end items-center space-x-12 mr-4">
+            <li>
+                <a href="../about">About</a>
+            </li>
+            <li>
+                <a href="../projects">Projects</a>
+            </li>
+            <li>
+                <a href="../blog">Blog</a>
+            </li>
+        </ul>
+    </nav>
+</div>
