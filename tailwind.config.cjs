bb70312d2b261b976105ff509f46e1fccb3314ec
Leon Wang <leonw2@illinois.edu>
2022-12-18 00:13:01 -0600

    configure tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}'],
 	theme: {
+		colors: {
+			'background' : '#f8f0e2',
+			'text' : '#292626',
+		},
+		fontFamily: {
+			'sans' : ['IBM Plex Mono'],
+			'mono' : ['Anonymous Pro', 'monospace']
+		},
 		extend: {},
 	},
 	plugins: [],
