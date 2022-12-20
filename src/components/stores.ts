bd73ef2534095c88f55db4ee70636994c090d7f8
Leon Wang <leonw2@illinois.edu>
2022-12-20 17:03:44 -0600

    reset

--- src/components/stores.ts
+++ src/components/stores.ts
@@ -1,19 +1,3 @@
-import { onDestroy } from 'svelte';
-import { readable } from 'svelte/store';
 import PocketBase from 'pocketbase';
 
-export const pb = readable(new PocketBase('http://127.0.0.1:8090'));
-
-export function getItem(item : string) : string {
-    var endpoint = "";
-    const unsubscribe = pb.subscribe(async function(instance : PocketBase) {
-        const record = await instance.collection('content').getOne(item);
-        endpoint = instance.getFileUrl(record, record.file);
-    });
-    console.log(endpoint);
-    if (endpoint == undefined) {
-        console.warn("Error: no item '" + item + "' exists in database.");
-    }
-    onDestroy(unsubscribe);
-    return endpoint;
-}
+export const pb = new PocketBase('http://127.0.0.1:8090');
