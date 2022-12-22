2160192e47811a5dfb08e8c044a8da2544b28abc
Leon Wang <leonw2@illinois.edu>
2022-12-22 15:16:28 -0600

    enforce type

--- src/components/pocketbase.ts
+++ src/components/pocketbase.ts
@@ -1,10 +1,10 @@
 import PocketBase from 'pocketbase';
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb : PocketBase = new PocketBase('http://127.0.0.1:8090');
 
 export async function getFile(fileID : string) {
-    const record = await pb.collection('content').getOne(fileID);
-    const url = pb.getFileUrl(record, record.file);
+    const record : any = await pb.collection('content').getOne(fileID);
+    const url : string = pb.getFileUrl(record, record.file);
     console.log(url);
     return url;
 }
