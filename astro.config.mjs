d1dc9cc9de08709a43fdb0a2c28507401d554f10
Leon Wang <leonw2@illinois.edu>
2022-12-22 17:35:26 -0600

    undo ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,4 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   // Enable Svelte to support Svelte components.
   integrations: [svelte(), tailwind()],
-  // Enable SSR rendering for PocketBase compatibility.
-  output: "server"
 });
