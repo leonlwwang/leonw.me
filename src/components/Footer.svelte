af099432fe068ad0ccf1ab99daa8526d1d6e716e
Leon Wang <leonw2@illinois.edu>
2022-12-28 02:40:59 -0600

    add footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,20 +1,68 @@
 <footer>
     <div id="handle"></div>
     <div id="links">
-        <a href="https://github.com/leonlwwang" target="_blank" rel="noreferrer">
-            <i class="icon">G</i>
+        <a href="https://github.com/leonlwwang/" target="_blank" rel="noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512">
+                <path d="M186.1 328.7c0 20.9-10.9 55.1-36.7 55.1s-36.7-34.2-36.7-55.1 
+                        10.9-55.1 36.7-55.1 36.7 34.2 36.7 55.1zM480 278.2c0 31.9-3.2 
+                        65.7-17.5 95-37.9 76.6-142.1 74.8-216.7 74.8-75.8 0-186.2 2.7
+                        -225.6-74.8-14.6-29-20.2-63.1-20.2-95 0-41.9 13.9-81.5 41.5
+                        -113.6-5.2-15.8-7.7-32.4-7.7-48.8 0-21.5 4.9-32.3 14.6-51.8 
+                        45.3 0 74.3 9 108.8 36 29-6.9 58.8-10 88.7-10 27 0 54.2 2.9 
+                        80.4 9.2 34-26.7 63-35.2 107.8-35.2 9.8 19.5 14.6 30.3 14.6 
+                        51.8 0 16.4-2.6 32.7-7.7 48.2 27.5 32.4 39 72.3 39 114.2zm-64.3 
+                        50.5c0-43.9-26.7-82.6-73.5-82.6-18.9 0-37 3.4-56 6-14.9 2.3-29.8 
+                        3.2-45.1 3.2-15.2 0-30.1-.9-45.1-3.2-18.7-2.6-37-6-56-6-46.8 0-73.5 
+                        38.7-73.5 82.6 0 87.8 80.4 101.3 150.4 101.3h48.2c70.3 0 150.6-13.4 
+                        150.6-101.3zm-82.6-55.1c-25.8 0-36.7 34.2-36.7 55.1s10.9 55.1 36.7 
+                        55.1 36.7-34.2 36.7-55.1-10.9-55.1-36.7-55.1z" fill="#2b2222"/>
+            </svg>
         </a>
         <a href="https://www.linkedin.com/in/leon-wang1/" target="_blank" rel="noreferrer">
-            <i class="icon">L</i>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 
+                        53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 
+                        448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 
+                        76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 
+                        94 0 111.28 61.9 111.28 142.3V448z" fill="#2b2222"/>
+            </svg>
         </a>
-        <a href="https://open.spotify.com/artist/4GV1B1I84BRLjPMU6DZLut" target="_blank" rel="noreferrer">
-            <i class="icon">S</i>
+        <a href="https://open.spotify.com/artist/4GV1B1I84BRLjPMU6DZLut/" target="_blank" rel="noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                <path d="M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 
+                        248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6
+                        -62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 
+                        0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 
+                        165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 
+                        15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9
+                        -238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4
+                        -19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 
+                        127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 
+                        19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5
+                        -198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3
+                        -10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6
+                        -15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 
+                        10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z" fill="#2b2222"/>
+            </svg>
         </a>
-        <a href="https://leonwang.itch.io/" target="_blank" rel="noreferrer">
-            <i class="icon">I</i>
+        <a href="https://www.instagram.com/leonlwwang/" target="_blank" rel="noreferrer">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M220.6 121.2L271.1 96 448 96v96H333.2c-21.9-15.1-48.5-24-77.2
+                        -24s-55.2 8.9-77.2 24H64V128H192c9.9 0 19.7-2.3 28.6-6.8zM0 
+                        128V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3
+                        -28.7-64-64-64H271.1c-9.9 0-19.7 2.3-28.6 6.8L192 64H160V48c0-8.8
+                        -7.2-16-16-16H80c-8.8 0-16 7.2-16 16l0 16C28.7 64 0 92.7 0 128zM344
+                         304c0 48.6-39.4 88-88 88s-88-39.4-88-88s39.4-88 88-88s88 39.4 88 88z" fill="#2b2222"/>
+            </svg>
         </a>
         <a href="mailto:leonw2@illinois.edu" target="_blank" rel="noreferrer">
-            <i class="icon">M</i>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 
+                        18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 
+                        12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 
+                        202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 
+                        311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z" fill="#2b2222"/>
+            </svg>
         </a>
     </div>
     <div>
@@ -55,14 +103,26 @@
         align-items: center;
         display: flex;
         margin-top: 0.5rem;
-        margin-bottom: 0.5rem;
+        margin-bottom: 1.5rem;
     }
-    .icon {
-        width: 3rem;
-        height: 3rem;
+    svg {
+        opacity: 0.88;
+        width: 1.5rem;
+        height: auto;
         display: inline-block;
-        margin-left: 0.5rem;
-        margin-right: 0.5rem;
+        margin-left: 1rem;
+        margin-right: 1rem;
+        transform: translateY(0px);
+        transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.75s;
+    }
+    svg:hover {
+        opacity: 1;
+        transform: translateY(-7px);
+        transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.225s;
     }
     p {
         font-weight: 400;
