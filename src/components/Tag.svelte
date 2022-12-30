8f29e06ff9fdcdd1d02907553315db3dc19a4903
Leon Wang <leonw2@illinois.edu>
2022-12-29 23:04:06 -0600

    initial commit

---
+++ src/components/Tag.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let tagName : string;
+</script>
+
+<a class="tag" href={"/tags/" + tagName}>{tagName}</a>
+
+<style>
+    .tag {
+		border: 1px;
+		border-style: solid;
+		border-color: #E6A595;
+		display: inline-block;
+		text-transform: uppercase;
+		font-size: 0.8rem;
+		line-height: 1;
+		padding: 0.2em 0.5em;
+		margin-right: 0.8em;
+		background-color: #E6A595;
+		text-decoration: none;
+		transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.5s;
+	}
+	.tag:hover {
+		border: 1px;
+		border-style: solid;
+		border-color: #2B2222;
+		background-color: inherit;
+		transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.5s;
+	}
+	a, a:visited {
+		text-decoration: none;
+		color: inherit;
+	}
+</style>
