94ec1890d0de7da5ac6c6b9ed57254a2b2ab6a03
Leon Wang <leonw2@illinois.edu>
2022-12-31 03:44:37 -0600

    not working

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,16 +1,42 @@
-<script lang="ts">
-
+<script>
 </script>
 
-<svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-    <g>
-      <path style="fill:#2B2222" d="M 7.48 8.126 L 7.281 25.375 L 7.205 40.909 
-                                    L 12.844 40.906 L 37.156 40.906 L 42.653 40.88 
-                                    L 42.719 8.219 L 7.48 8.126 Z"/>
-    </g>
-</svg>
+{#if true}
+    <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <path>
+            <animate 
+                attributeName="d"
+                repeatCount="1"
+                fill="freeze"
+                dur="0.5s"
+                values="M 7.48 8.126 L 7.281 25.375 L 7.205 40.909 L 12.844 40.906 L 37.156 40.906 L 42.653 40.88 L 42.719 8.219 L 7.48 8.126 Z;
+                        M 24.974 8.126 L 7.281 25.375 L 13.003 25.377 L 12.844 40.906 L 37.156 40.906 L 37.022 24.901 L 42.719 24.872 L 24.974 8.126 Z"
+            />
+          </path>
+        </g>
+    </svg>
+{:else}
+    <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+        <g>
+          <path>
+            <animate 
+                attributeName="d"
+                repeatCount="1"
+                fill="freeze"
+                dur="0.333s"
+                values="M 24.974 8.126 L 7.281 25.375 L 13.003 25.377 L 12.844 40.906 L 37.156 40.906 L 37.022 24.901 L 42.719 24.872 L 24.974 8.126 Z;
+                        M 7.48 8.126 L 7.281 25.375 L 7.205 40.909 L 12.844 40.906 L 37.156 40.906 L 42.653 40.88 L 42.719 8.219 L 7.48 8.126 Z"
+            />
+          </path>
+        </g>
+    </svg>
+{/if}
 
 <style>
+    path {
+        fill: #2B2222;
+    }
     svg {
         justify-self: center;
         position: relative;
