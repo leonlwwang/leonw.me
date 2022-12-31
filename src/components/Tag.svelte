947f9d50b5cdb98d6673c12d5dcb470f3d24313d
Leon Wang <leonw2@illinois.edu>
2022-12-30 19:40:51 -0600

    colored tags

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -2,24 +2,25 @@
     export let tagName : string;
 </script>
 
-<a class="tag" href={"/tags/" + tagName}>{tagName}</a>
+{#if tagName == "first post"}
+	<a class="tag" class:tag1={true} href={"/tags/" + tagName}>{tagName}</a>
+{:else if tagName == "rambling"}
+	<a class="tag" class:tag2={true} href={"/tags/" + tagName}>{tagName}</a>
+{:else if tagName == "music production"}
+	<a class="tag" class:tag3={true} href={"/tags/" + tagName}>{tagName}</a>
+{:else if tagName == "game development"}
+	<a class="tag" class:tag4={true} href={"/tags/" + tagName}>{tagName}</a>
+{/if}
 
 <style>
     .tag {
-		border: 1px;
-		border-style: solid;
-		border-color: #E6A595;
 		display: inline-block;
 		text-transform: uppercase;
 		font-size: 0.8rem;
 		line-height: 1;
 		padding: 0.2em 0.5em;
-		margin-right: 0.8em;
-		background-color: #E6A595;
+		margin-right: 0em;
 		text-decoration: none;
-		transition-property: all;
-        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-        transition-duration: 0.5s;
 	}
 	.tag:hover {
 		border: 1px;
@@ -33,5 +34,45 @@
 	a, a:visited {
 		text-decoration: none;
 		color: inherit;
+	}
+	/* FIRST POST tag */
+	.tag1 {
+		border: 1px;
+		border-style: solid;
+		border-color: white;
+		background-color: white;
+		transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.5s;
+	}
+	/* RAMBLING tag */
+	.tag2 {
+		border: 1px;
+		border-style: solid;
+		border-color: #FDFD96;
+		background-color: #FDFD96;
+		transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.5s;
+	}
+	/* MUSIC PROD. tag */
+	.tag3 {
+		border: 1px;
+		border-style: solid;
+		border-color: #FFC0CB;
+		background-color: #FFC0CB;
+		transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.5s;
+	}
+	/* GAME DEV tag */
+	.tag4 {
+		border: 1px;
+		border-style: solid;
+		border-color: #ACE1AF;
+		background-color: #ACE1AF;
+		transition-property: all;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-duration: 0.5s;
 	}
 </style>
